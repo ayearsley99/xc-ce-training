d188e492d843da95823254bbb7e35d0fb1f303e6
ayearsley99 <a.yearsley@f5.com>
2025-04-23 16:46:23 +0100

    AY smsv2.tf mod v2

--- smsv2.tf
+++ smsv2.tf
@@ -7,7 +7,7 @@ resource "volterra_securemesh_site_v2" "site" {
   logs_streaming_disabled = true
   enable_ha               = false
   labels = {
-    (virtual-site-terraform) = (volterra_known_label.label.value)
+    ("virtual-site-terraform") = (volterra_known_label.label.value)
     "ves.io/provider"                  = "ves-io-AZURE"
   }
 
